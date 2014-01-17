ad028d2a8363067f05a66769e3fe055dfc8df8bf
dan-lyn <daniel_lynge@hotmail.com>
2014-01-17 14:17:29 +0100

    check if $id is a number

--- index.php
+++ index.php
@@ -48,28 +48,32 @@ $app->get('/items', function() use($app) {
  * GET a single item
  */
 $app->get('/item/:id', function($id) use($app) {
-  $db = getConnection($app->config('custom'));
-  $arrItem = array();
+  if($id>0 && is_numeric($id)) {
+    $db = getConnection($app->config('custom'));
+    $arrItem = array();
 
-  $sql = "SELECT * FROM items WHERE id=".$db->real_escape_string($id);
-  $res = $db->query($sql);
-  if($res && $res->num_rows>0) {
-    $arrItem = $res->fetch_array(MYSQLI_ASSOC);
+    $sql = "SELECT * FROM items WHERE id=".$db->real_escape_string($id);
+    $res = $db->query($sql);
+    if($res && $res->num_rows>0) {
+      $arrItem = $res->fetch_array(MYSQLI_ASSOC);
+    }
+
+    echo json_encode($arrItem);
   }
-
-  echo json_encode($arrItem);
 });
 
 /**
  * PUT (update) a single item
  */
 $app->put('/item/:id', function($id) use($app) {
-  $db = getConnection($app->config('custom'));
+  if($id>0 && is_numeric($id)) {
+    $db = getConnection($app->config('custom'));
 
-  $request = json_decode($app->request->getBody());
+    $request = json_decode($app->request->getBody());
   
-  $sql = "UPDATE items SET active=".$db->real_escape_string($request->active)." WHERE id=".$db->real_escape_string($id);
-  $db->query($sql);
+    $sql = "UPDATE items SET active=".$db->real_escape_string($request->active)." WHERE id=".$db->real_escape_string($id);
+    $db->query($sql);
+  }
 });
 
 
