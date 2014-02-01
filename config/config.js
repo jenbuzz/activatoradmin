e0c658624fefca4daf3fd101497593168a1fad67
dan-lyn <daniel_lynge@hotmail.com>
2014-02-01 14:07:27 +0100

    added configurable db table structure mapping if table structure changes/differs

--- config/config.js
+++ config/config.js
@@ -1,3 +1,8 @@
 var appConfig = {
-  imagePath: 'images/'
+  imagePath: 'images/',
+  db_mapping: {
+    name: 'name',
+    isactive: 'isactive',
+    image: 'image'
+  }
 }
--- js/activatoradmin.js
+++ js/activatoradmin.js
@@ -4,7 +4,20 @@ $(function(){
   // Model
   var ActivatorItem = Backbone.Model.extend({
     urlRoot: 'index.php/item',
-    idAttribute: 'id'
+    idAttribute: 'id',
+    initialize: function() {
+      if( typeof appConfig != 'undefined' && appConfig.hasOwnProperty('db_mapping') ) {
+        if( appConfig.db_mapping.hasOwnProperty('name') && appConfig.db_mapping.name!='' ) {
+          this.set('name', this.get(appConfig.db_mapping.name));
+        }
+        if( appConfig.db_mapping.hasOwnProperty('isactive') && appConfig.db_mapping.isactive!='' ) {
+          this.set('isactive', this.get(appConfig.db_mapping.isactive));
+        }
+        if( appConfig.db_mapping.hasOwnProperty('image') && appConfig.db_mapping.image!='' ) {
+          this.set('image', this.get(appConfig.db_mapping.image));
+        }
+      }
+    }
   });
 
   // Collection
