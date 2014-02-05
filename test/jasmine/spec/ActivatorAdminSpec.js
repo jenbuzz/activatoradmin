e95fbb21aa2c5b2dfa15796eb98a57ecc0ddc488
dan-lyn <daniel_lynge@hotmail.com>
2014-02-05 15:39:20 +0100

    jasmine testing the existence of needed model/collection/view classes

---
+++ test/jasmine/spec/ActivatorAdminSpec.js
@@ -0,0 +1,34 @@
+(function() {
+
+  describe("ActivatorAdmin Model", function() {
+    it("should exist", function() {
+      return expect(ActivatorItem).toBeDefined();
+    });
+  });
+
+  describe("ActivatorAdmin Collection", function() {
+    it("should exist", function() {
+      return expect(ActivatorList).toBeDefined();
+    });
+  });
+
+  describe("ActivatorAdmin ItemView", function() {
+    it("should exist", function() {
+      return expect(ActivatorItemView).toBeDefined();
+    });
+  });
+
+  describe("ActivatorAdmin PaginationView", function() {
+    it("should exist", function() {
+      return expect(ActivatorPaginationView).toBeDefined();
+    });
+  });
+
+  describe("ActivatorAdmin AppView", function() {
+    it("should exist", function() {
+      return expect(ActivatorAppView).toBeDefined();
+    });
+  });
+
+}).call(this);
+
