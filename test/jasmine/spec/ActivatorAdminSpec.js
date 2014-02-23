7e3a2c01c92c1ad0bd8e82a2fde4d243f70e4683
dan-lyn <daniel_lynge@hotmail.com>
2014-02-23 11:21:46 +0100

    testing that configs on model are set

--- test/jasmine/spec/ActivatorAdminSpec.js
+++ test/jasmine/spec/ActivatorAdminSpec.js
@@ -4,6 +4,14 @@
     it("should exist", function() {
       return expect(ActivatorItem).toBeDefined();
     });
+    it("should not have an empty string urlRoot", function() {
+      var item = new ActivatorItem();
+      return expect(item.get('urlRoot')).not.toBe("");
+    });
+    it("should not have an empty string idAttribute", function() {
+      var item = new ActivatorItem();
+      return expect(item.get('idAttribute')).not.toBe("");
+    });
   });
 
   describe("ActivatorAdmin Collection", function() {
