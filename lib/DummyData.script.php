337a237305a85d611e1ca2424f062d77b7272825
dan-lyn <daniel_lynge@hotmail.com>
2014-02-26 13:07:32 +0100

    fix dummydatascript to match updated db functionality

--- lib/DummyData.script.php
+++ lib/DummyData.script.php
@@ -4,8 +4,7 @@ require_once(__DIR__.'/../config/config.php');
 require_once(__DIR__.'/DB.class.php');
 require_once(__DIR__.'/Faker/autoload.php');
 
-$objDB = \ActivatorAdmin\Lib\DB::getInstance();
-$db = $objDB->getConnection($config);
+$objDB = \ActivatorAdmin\Lib\DB::getInstance($config['db']);
 
 $objFaker = \Faker\Factory::create();
 
@@ -14,7 +13,7 @@ for ($i=0; $i<50; $i++) {
     if ($i%3) {
       $isactive = 1;
     }
-    $sql = "INSERT INTO {$config['db']['table']} (isactive, name) VALUES ({$isactive}, \"{$objFaker->name}\")";
-    $db->query($sql);
+    //$sql = "INSERT INTO {$config['db']['table']} (isactive, name) VALUES ({$isactive}, \"{$objFaker->name}\")";
+    $objDB->insert($config['db']['table'], array('isactive'=>$isactive, 'name'=>$objFaker->name));
 }
 
