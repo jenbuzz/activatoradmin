398a1c29d84f50f4709e38bdc2746b775bfff2ed
Lynge <daniel_lynge@hotmail.com>
2014-03-23 18:55:41 +0100

    ConfigHelper class (not done yet)

---
+++ lib/ConfigHelper.class.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ActivatorAdmin\Lib;
+
+class ConfigHelper
+{
+    private $config;
+
+    private function __construct()
+	{
+	    // TODO: load config file
+	}
+
+    public function get(String $key)
+	{
+	    // TODO: check if $key is in $config and return
+	}
+
+}
