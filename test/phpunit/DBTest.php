a64509e787679f979b32ae4c6a5f750918262183
dan-lyn <daniel_lynge@hotmail.com>
2014-04-03 10:47:14 +0200

    refactoring + code documentation

--- test/phpunit/DBTest.php
+++ test/phpunit/DBTest.php
@@ -15,6 +15,7 @@ use \ActivatorAdmin\Lib\DB;
 class DBTest extends \PHPUnit_Framework_TestCase
 {
     protected $db;
+    protected $dbConfig;
 
     /**
      * Connects to a MySQL database and gets an instance of DB.
@@ -24,12 +25,12 @@ class DBTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $objConfigHelper = new ConfigHelper();
-        $dbConfig = $objConfigHelper->get('db');
-        $this->db = DB::getInstance($dbConfig);
+        $this->dbConfig = $objConfigHelper->get('db');
+        $this->db = DB::getInstance($this->dbConfig);
 
         // Create pseudo table for testing
         $mysqli = $this->db->getConnection();
-        $sql = "CREATE TABLE IF NOT EXISTS ".$dbConfig['table']."_test ";
+        $sql = "CREATE TABLE IF NOT EXISTS ".$this->dbConfig['table']."_test ";
         $sql.= "(id INT(11) NOT NULL AUTO_INCREMENT, isactive TINYINT(4), name VARCHAR(255), image VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))";
         $mysqli->query($sql);
     }
@@ -39,13 +40,11 @@ class DBTest extends \PHPUnit_Framework_TestCase
      */
     protected function tearDown()
     {
-        $objConfigHelper = new ConfigHelper();
-        $dbConfig = $objConfigHelper->get('db');
-        $this->db = DB::getInstance($dbConfig);
+        $this->db = DB::getInstance($this->dbConfig);
 
         // Create pseudo table for testing
         $mysqli = $this->db->getConnection();
-        $sql = "DROP TABLE ".$dbConfig['table']."_test";
+        $sql = "DROP TABLE ".$this->dbConfig['table']."_test";
         $mysqli->query($sql);
     }
 
@@ -66,19 +65,20 @@ class DBTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('mysqli', $mysqli);
     }
 
+    /**
+     * Test select function.
+     * First inserting a record into the test table and then testing the size of the test table.
+     */
     public function testSelect()
     {
-        $objConfigHelper = new ConfigHelper();
-        $dbConfig = $objConfigHelper->get('db');
-        $this->db = DB::getInstance($dbConfig);
         $mysqli = $this->db->getConnection();
 
         // Insert test record
-        $sqlInsert = "INSERT INTO ".$dbConfig['table']."_test (isactive, name) VALUES (1, 'Test Record 1')";
+        $sqlInsert = "INSERT INTO ".$this->dbConfig['table']."_test (isactive, name) VALUES (1, 'Test Record 1')";
         $mysqli->query($sqlInsert);
 
         // Test select function
-        $result = $this->db->select($dbConfig['table']."_test");
+        $result = $this->db->select($this->dbConfig['table']."_test");
         $this->assertGreaterThan(0, sizeof($result));
     }
 
