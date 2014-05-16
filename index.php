f3afca7b72a3b22426a7cad6ca5e13eddaa7af57
dan-lyn <daniel_lynge@hotmail.com>
2014-05-16 10:18:21 +0200

    checking for logged in user in all routes

--- index.php
+++ index.php
@@ -61,26 +61,40 @@ $app->post('/login', function() use($app) {
  * GET all items
  */
 $app->get('/items', function() use($app, $dbConfig) {
-    $objDB = DB::getInstance($dbConfig);
-    
-    $arrItems = $objDB->select($dbConfig['table']);
+    if (!isset($_SESSION['activatoradmin_user'])) {
+        $objConfigHelper = $app->config('custom');
+        $baseurl = $objConfigHelper->get('url', 'baseurl');
 
-    echo json_encode($arrItems);
+        $app->redirect($baseurl.'index.php/login');
+    } else {
+        $objDB = DB::getInstance($dbConfig);
+    
+        $arrItems = $objDB->select($dbConfig['table']);
+
+        echo json_encode($arrItems);
+    }
 });
 
 /**
  * GET a single item
  */
 $app->get('/item/:id', function($id) use($app, $dbConfig) {
-    $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
-    if ($id>0 && is_numeric($id)) {
-        $objDB = DB::getInstance($dbConfig);
-        
-	$arrItem = $objDB->select($dbConfig['table'], '*', 'id', $id);
-        
-        echo json_encode($arrItem);
+    if (!isset($_SESSION['activatoradmin_user'])) {
+        $objConfigHelper = $app->config('custom');
+        $baseurl = $objConfigHelper->get('url', 'baseurl');
+
+        $app->redirect($baseurl.'index.php/login');
     } else {
-        echo json_encode(array('success'=>false));
+        $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+        if ($id>0 && is_numeric($id)) {
+            $objDB = DB::getInstance($dbConfig);
+        
+            $arrItem = $objDB->select($dbConfig['table'], '*', 'id', $id);
+        
+            echo json_encode($arrItem);
+        } else {
+            echo json_encode(array('success'=>false));
+        }
     }
 });
 
@@ -88,19 +102,26 @@ $app->get('/item/:id', function($id) use($app, $dbConfig) {
  * PUT (update) a single item
  */
 $app->put('/item/:id', function($id) use($app, $dbConfig) {
-    $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
-    if ($id>0 && is_numeric($id)) {
-        $objDB = DB::getInstance($dbConfig);
+    if (!isset($_SESSION['activatoradmin_user'])) {
+        $objConfigHelper = $app->config('custom');
+        $baseurl = $objConfigHelper->get('url', 'baseurl');
 
-        $request = json_decode($app->request->getBody());
+        $app->redirect($baseurl.'index.php/login');
+    } else {
+        $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+        if ($id>0 && is_numeric($id)) {
+            $objDB = DB::getInstance($dbConfig);
+
+            $request = json_decode($app->request->getBody());
  
-        if (is_object($request) && isset($request->isactive)) { 
-            $objDB->update($dbConfig['table'], array('isactive'=>$request->isactive), 'id', $id);
+            if (is_object($request) && isset($request->isactive)) { 
+                $objDB->update($dbConfig['table'], array('isactive'=>$request->isactive), 'id', $id);
+            } else {
+                echo json_encode(array('success'=>false));
+            }
         } else {
             echo json_encode(array('success'=>false));
         }
-    } else {
-        echo json_encode(array('success'=>false));
     }
 });
 
@@ -108,15 +129,22 @@ $app->put('/item/:id', function($id) use($app, $dbConfig) {
  * DELETE a single item
  */
 $app->delete('/item/:id', function($id) use($app, $dbConfig) {
-    $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
-    if ($id>0 && is_numeric($id)) {
-        $objDB = DB::getInstance($dbConfig);
+    if (!isset($_SESSION['activatoradmin_user'])) {
+        $objConfigHelper = $app->config('custom');
+        $baseurl = $objConfigHelper->get('url', 'baseurl');
 
-        $objDB->delete($dbConfig['table'], 'id', $id);
-
-        echo json_encode(array('success'=>true));
+        $app->redirect($baseurl.'index.php/login');
     } else {
-        echo json_encode(array('success'=>false));
+        $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+        if ($id>0 && is_numeric($id)) {
+            $objDB = DB::getInstance($dbConfig);
+
+            $objDB->delete($dbConfig['table'], 'id', $id);
+
+            echo json_encode(array('success'=>true));
+        } else {
+            echo json_encode(array('success'=>false));
+        }
     }
 });
 
