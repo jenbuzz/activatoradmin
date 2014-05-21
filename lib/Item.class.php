bd24511f929660b329c16078556268df4e52fb59
dan-lyn <daniel_lynge@hotmail.com>
2014-05-21 21:35:28 +0200

    initial save and delete functions updated

--- lib/Item.class.php
+++ lib/Item.class.php
@@ -1,25 +1,43 @@
 <?php
 
+require_once(__DIR__ . '/DB.class.php');
+require_once(__DIR__ . '/ConfigHelper.class.php');
+
 namespace ActivatorAdmin\Lib;
+
+use \ActivatorAdmin\Lib\DB;
+use \ActivatorAdmin\Lib\ConfigHelper;
 
 class Item
 {
-
+    private $objDB, $table;
     protected $id, $name, $isActive, $image;
+
+    public function __construct()
+    {
+        $objConfigHelper = new ConfigHelper();
+        $dbConfig = $objConfigHelper->get('db');
+
+        $this->objDB = DB::getInstance($dbConfig);
+        $this->table = $dbConfig['table'];
+    }
 
     public function save()
     {
-        // TODO
+        if ($this->id) {
+            $this->objDB->update($this->table, array('isactive'=>$this->getIsActive()), 'id', $this->getId());
+        } else {
+            $this->objDB->insert($this->table, array(
+                'name'=>$this->getName(),
+                'isactive'=>$this->getIsActive(),
+                'image'=>$this->getImage()
+            ));
+        }
     }
 
     public function delete()
     {
-        // TODO
-    }
-
-    public function setId($id)
-    {
-        $this->id = $id;
+        $this->objDB->delete($this->table, 'id', $this->getId());
     }
 
     public function getId()
