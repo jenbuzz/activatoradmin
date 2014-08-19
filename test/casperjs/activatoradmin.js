7e81ac19f8ade4c5625836dfc2e2fc329d925496
dan-lyn <daniel_lynge@hotmail.com>
2014-08-19 20:21:16 +0200

    casperjs test case for login

--- test/casperjs/activatoradmin.js
+++ test/casperjs/activatoradmin.js
@@ -1,9 +1,35 @@
-casper.test.begin('Title is ActivatorAdmin', 1, function suite(test) {
+phantom.injectJs("config/config.js");
 
-  phantom.injectJs("config/config.js");
+casper.test.begin('Title is ActivatorAdmin', 1, function suite(test) {
 
   casper.start('http://'+appConfig.host+appConfig.baseUrl, function() {
     test.assertTitle('ActivatorAdmin');
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+
+});
+
+casper.test.begin('Login is working', 2, function suite(test) {
+
+  var url = 'http://'+appConfig.host+appConfig.baseUrl;
+  var urlLogin = url+'login';
+
+  casper.start(url, function() {
+    test.assertEquals(this.getCurrentUrl(), urlLogin);
+
+    this.fill('form[action="/activatoradmin/login"]', {
+      'username': 'admin',
+      'password': 'admin',
+    }, true);
+
+    this.wait(200);
+  });
+
+  casper.then(function() {
+    test.assertEquals(this.getCurrentUrl(), url);
   });
 
   casper.run(function() {
