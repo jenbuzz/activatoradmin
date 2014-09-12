fcbe3e58465d94f72f41efb7a2e6305a11ee7270
dan-lyn <daniel_lynge@hotmail.com>
2014-09-12 11:09:04 +0200

    refactored authentication check; now using simple middleware function

--- index.php
+++ index.php
@@ -18,18 +18,26 @@ $app = new \Slim\Slim(
 
 $app->add(new \Slim\Middleware\SessionCookie(array('secret' => 'Aa-Secret')));
 
+// Authentication check: redirect to login page if user is not logged in.
+$authenticate = function($app) {
+    return function() use ($app) {
+        $objConfigHelper = $app->config('custom');
+        $baseurl = $objConfigHelper->get('url', 'baseurl');
+
+        if (!isset($_SESSION['activatoradmin_user'])) {
+            $app->redirect($baseurl.'login');
+        }
+    };
+};
+
 /**
  * Render startup template (index)
  */
-$app->get('/', function() use($app) {
+$app->get('/', $authenticate($app), function() use($app) {
     $objConfigHelper = $app->config('custom');
     $baseurl = $objConfigHelper->get('url', 'baseurl');
 
-    if (!isset($_SESSION['activatoradmin_user'])) {
-        $app->redirect($baseurl.'login');
-    } else {
-        $app->render('index.tpl', array('baseurl'=>$baseurl));
-    }
+    $app->render('index.tpl', array('baseurl'=>$baseurl));
 });
 
 /**
@@ -68,120 +76,85 @@ $app->get('/logout', function() use($app) {
 /**
  * GET all items
  */
-$app->get('/items', function() use($app) {
-    if (!isset($_SESSION['activatoradmin_user'])) {
-        $objConfigHelper = $app->config('custom');
-        $baseurl = $objConfigHelper->get('url', 'baseurl');
+$app->get('/items', $authenticate($app), function() use($app) {
+    $arrItems = array();
 
-        $app->redirect($baseurl.'login');
-    } else {
-        $arrItems = array();
-
-        $objModelFacade = new ModelFacade(new Item());
-        $arrItemObjects = $objModelFacade->loadAll();
-        foreach ($arrItemObjects as $objItem) {
-            $arrItems[] = $objItem->toArray();
-        }
-
-        echo json_encode($arrItems);
+    $objModelFacade = new ModelFacade(new Item());
+    $arrItemObjects = $objModelFacade->loadAll();
+    foreach ($arrItemObjects as $objItem) {
+        $arrItems[] = $objItem->toArray();
     }
+
+    echo json_encode($arrItems);
 });
 
 /**
  * GET a single item
  */
-$app->get('/item/:id', function($id) use($app) {
-    if (!isset($_SESSION['activatoradmin_user'])) {
-        $objConfigHelper = $app->config('custom');
-        $baseurl = $objConfigHelper->get('url', 'baseurl');
-
-        $app->redirect($baseurl.'login');
-    } else {
-        $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
-        if ($id>0 && is_numeric($id)) {
-            $objModelFacade = new ModelFacade(new Item());
-            $objItem = $objModelFacade->load($id);
+$app->get('/item/:id', $authenticate($app), function($id) use($app) {
+    $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+    if ($id>0 && is_numeric($id)) {
+        $objModelFacade = new ModelFacade(new Item());
+        $objItem = $objModelFacade->load($id);
         
-            echo json_encode($objItem->toArray());
-        } else {
-            echo json_encode(array('success'=>false));
-        }
+        echo json_encode($objItem->toArray());
+    } else {
+        echo json_encode(array('success'=>false));
     }
 });
 
 /**
  * PUT (update) a single item
  */
-$app->put('/item/:id', function($id) use($app) {
-    if (!isset($_SESSION['activatoradmin_user'])) {
-        $objConfigHelper = $app->config('custom');
-        $baseurl = $objConfigHelper->get('url', 'baseurl');
-
-        $app->redirect($baseurl.'login');
-    } else {
-        $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
-        if ($id>0 && is_numeric($id)) {
-            $request = json_decode($app->request->getBody());
+$app->put('/item/:id', $authenticate($app), function($id) use($app) {
+    $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+    if ($id>0 && is_numeric($id)) {
+        $request = json_decode($app->request->getBody());
  
-            if (is_object($request) && isset($request->isactive)) {
-                $objModelFacade = new ModelFacade(new Item());
-                $objItem = $objModelFacade->load($id);
-                $objItem->setIsActive($request->isactive);
-                $objItem->save();
-            } else {
-                echo json_encode(array('success'=>false));
-            }
+        if (is_object($request) && isset($request->isactive)) {
+            $objModelFacade = new ModelFacade(new Item());
+            $objItem = $objModelFacade->load($id);
+            $objItem->setIsActive($request->isactive);
+            $objItem->save();
         } else {
             echo json_encode(array('success'=>false));
         }
+    } else {
+        echo json_encode(array('success'=>false));
     }
 });
 
 /**
  * DELETE a single item
  */
-$app->delete('/item/:id', function($id) use($app) {
-    if (!isset($_SESSION['activatoradmin_user'])) {
-        $objConfigHelper = $app->config('custom');
-        $baseurl = $objConfigHelper->get('url', 'baseurl');
+$app->delete('/item/:id', $authenticate($app), function($id) use($app) {
+    $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+    if ($id>0 && is_numeric($id)) {
+        $objModelFacade = new ModelFacade(new Item());
+        $objModelFacade->load($id);
+        $objModelFacade->delete();
 
-        $app->redirect($baseurl.'login');
+        echo json_encode(array('success'=>true));
     } else {
-        $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
-        if ($id>0 && is_numeric($id)) {
-            $objModelFacade = new ModelFacade(new Item());
-            $objModelFacade->load($id);
-            $objModelFacade->delete();
-
-            echo json_encode(array('success'=>true));
-        } else {
-            echo json_encode(array('success'=>false));
-        }
+        echo json_encode(array('success'=>false));
     }
 });
 
 /**
  * GET search items
  */
-$app->get('/search/:term', function($term) use($app) {
-    if (!isset($_SESSION['activatoradmin_user'])) {
-        $objConfigHelper = $app->config('custom');
-        $baseurl = $objConfigHelper->get('url', 'baseurl');
-
-        $app->redirect($baseurl.'login');
-    } else {
-        $term = filter_var($term, FILTER_SANITIZE_STRING);
+$app->get('/search/:term', $authenticate($app), function($term) use($app) {
+    $term = filter_var($term, FILTER_SANITIZE_STRING);
         
-        $arrItems = array();
+    $arrItems = array();
 
-        $objModelFacade = new ModelFacade(new Item());
-        $arrItemObjects = $objModelFacade->search($term);
-        foreach ($arrItemObjects as $objItem) {
-            $arrItems[] = $objItem->toArray();
-        }
-
-        echo json_encode($arrItems);
+    $objModelFacade = new ModelFacade(new Item());
+    $arrItemObjects = $objModelFacade->search($term);
+    foreach ($arrItemObjects as $objItem) {
+        $arrItems[] = $objItem->toArray();
     }
+
+    echo json_encode($arrItems);
 });
 
 
