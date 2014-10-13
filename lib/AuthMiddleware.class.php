116b5cf831fe079bb5d6de29e25f3688d0eeb026
dan-lyn <daniel_lynge@hotmail.com>
2014-10-13 19:52:28 +0200

    code documentation

--- lib/AuthMiddleware.class.php
+++ lib/AuthMiddleware.class.php
@@ -1,9 +1,18 @@
 <?php
-
+/**
+ * AuthMiddleware is a custom Slim Middleware class used for authentication.
+ * It is used in index.php during setting up the Slim app.
+ *
+ */
 namespace ActivatorAdmin\Lib;
 
 class AuthMiddleware extends \Slim\Middleware
 {
+  /**
+   * Function from the abstract class  \Slim\Middleware.
+   * Using the hook "slim.before.dispatch" it is being verified if the use is logged in.
+   * The session variable 'activatoradmin_user' is being checked.
+   */
   public function call()
   {
       $authenticate = function($app) {
