9f24dd0ca2cfb7300d4cf3a219ee810a908009b4
dan-lyn <daniel_lynge@hotmail.com>
2014-10-18 17:35:10 +0200

    setup autoload to handle psr log and monolog classes

--- lib/autoload.php
+++ lib/autoload.php
@@ -4,7 +4,11 @@
  * ActivatorAdmin
  */
 spl_autoload_register(function($class) {
-    if (substr($class, 0, 4)!=='Slim') {
+    if (substr($class, 0, 7)=='Monolog') {
+        require __DIR__ . '/monolog/src/Monolog/' . str_replace("Monolog\\", '', $class) . '.php';
+    } elseif (substr($class, 0, 3)=='Psr') {
+        require __DIR__ . '/log/Psr/Log/' . str_replace("Psr\\Log\\", '', $class) . '.php';
+    } elseif (substr($class, 0, 4)!=='Slim') {
         $class = str_replace('ActivatorAdmin\\Lib\\', '', $class);
 
         if (substr($class, 0, 1)==='i') {
