d5d9ec56211424c5bcf3011e202971c43a83d861
dan-lyn <daniel_lynge@hotmail.com>
2015-11-21 10:03:46 +0100

    moved js code into function and set to strict mode

--- js/stats.js
+++ js/stats.js
@@ -1,54 +1,58 @@
-var margin = {
-  top: 20, 
-  right: 20, 
-  bottom: 30, 
-  left: 40
-};
+(function ($) {
+  "use strict";
 
-var width = 960 - margin.left - margin.right;
-var height = 500 - margin.top - margin.bottom;
+  var margin = {
+    top: 20, 
+    right: 20, 
+    bottom: 30, 
+    left: 40
+  };
 
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-var y = d3.scale.linear().range([height, 0]);
+  var width = 960 - margin.left - margin.right;
+  var height = 500 - margin.top - margin.bottom;
 
-var xAxis = d3.svg.axis().scale(x).orient("bottom");
-var yAxis = d3.svg.axis().scale(y).orient("left").ticks(5);
+  var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
+  var y = d3.scale.linear().range([height, 0]);
 
-var svg = d3.select(".statsActivated").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var xAxis = d3.svg.axis().scale(x).orient("bottom");
+  var yAxis = d3.svg.axis().scale(y).orient("left").ticks(5);
 
-d3.json("get-stats", function(error, data) {
-  if (error) {
-    throw error;
-  }
+  var svg = d3.select(".statsActivated").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  x.domain(data.map(function(d) { return d.name; }));
-  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+  d3.json("get-stats", function(error, data) {
+    if (error) {
+      throw error;
+    }
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+    x.domain(data.map(function(d) { return d.name; }));
+    y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Number of items");
+    svg.append("g")
+       .attr("class", "x axis")
+       .attr("transform", "translate(0," + height + ")")
+       .call(xAxis);
 
-   svg.selectAll(".bar")
-      .data(data)
-      .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.name); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); });
-});
+    svg.append("g")
+       .attr("class", "y axis")
+       .call(yAxis)
+       .append("text")
+       .attr("transform", "rotate(-90)")
+       .attr("y", 6)
+       .attr("dy", ".71em")
+       .style("text-anchor", "end")
+       .text("Number of items");
+
+    svg.selectAll(".bar")
+       .data(data)
+       .enter().append("rect")
+       .attr("class", "bar")
+       .attr("x", function(d) { return x(d.name); })
+       .attr("width", x.rangeBand())
+       .attr("y", function(d) { return y(d.value); })
+       .attr("height", function(d) { return height - y(d.value); });
+  });
+}(jQuery));
