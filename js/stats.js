8f2778ce131a2ce86250341f121cceb2df7f0970
dan-lyn <daniel_lynge@hotmail.com>
2015-11-29 10:12:15 +0100

    coding std fixes

--- js/stats.js
+++ js/stats.js
@@ -1,5 +1,5 @@
 (function ($) {
-  "use strict";
+  'use strict';
 
   var margin = {
     top: 20, 
@@ -14,16 +14,17 @@
   var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
   var y = d3.scale.linear().range([height, 0]);
 
-  var xAxis = d3.svg.axis().scale(x).orient("bottom");
-  var yAxis = d3.svg.axis().scale(y).orient("left").ticks(5);
+  var xAxis = d3.svg.axis().scale(x).orient('bottom');
+  var yAxis = d3.svg.axis().scale(y).orient('left').ticks(5);
 
-  var svg = d3.select(".statsActivated").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var svg = d3.select('.statsActivated')
+      .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-  d3.json("get-stats", function(error, data) {
+  d3.json('get-stats', function(error, data) {
     if (error) {
       throw error;
     }
@@ -31,30 +32,31 @@
     x.domain(data.map(function(d) { return d.name; }));
     y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-    svg.append("g")
-       .attr("class", "x axis")
-       .attr("transform", "translate(0," + height + ")")
+    svg.append('g')
+       .attr('class', 'x axis')
+       .attr('transform', 'translate(0,' + height + ')')
        .call(xAxis);
 
-    svg.append("g")
-       .attr("class", "y axis")
+    svg.append('g')
+       .attr('class', 'y axis')
        .call(yAxis)
-       .append("text")
-       .attr("transform", "rotate(-90)")
-       .attr("y", 6)
-       .attr("dy", ".71em")
-       .style("text-anchor", "end")
-       .text("Number of items");
+       .append('text')
+       .attr('transform', 'rotate(-90)')
+       .attr('y', 6)
+       .attr('dy', '.71em')
+       .style('text-anchor', 'end')
+       .text('Number of items');
 
-    svg.selectAll(".bar")
+    svg.selectAll('.bar')
        .data(data)
-       .enter().append("rect")
-       .attr("class", "bar")
-       .attr("x", function(d) { return x(d.name); })
-       .attr("width", x.rangeBand())
-       .attr("y", function(d) { return y(d.value); })
-       .attr("height", function(d) { return height - y(d.value); })
-       .append("title")
-       .text(function(d) { return d.value;});
+       .enter()
+       .append('rect')
+       .attr('class', 'bar')
+       .attr('x', function(d) { return x(d.name); })
+       .attr('width', x.rangeBand())
+       .attr('y', function(d) { return y(d.value); })
+       .attr('height', function(d) { return height - y(d.value); })
+       .append('title')
+       .text(function(d) { return d.value; });
   });
 }(jQuery));
