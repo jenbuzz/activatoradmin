a1a46edfa5ecd5adb3bb79eb4dfe30347029351c
dan-lyn <daniel_lynge@hotmail.com>
2016-08-14 11:04:36 +0200

    unittest for db class

---
+++ test/phpunit/DBTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ActivatorAdmin\Test\PHPunit;
+
+require_once __DIR__ . '/../../lib/autoload.php';
+
+use ActivatorAdmin\Lib\ConfigHelper;
+use ActivatorAdmin\Lib\DB;
+
+/**
+ * Test the DB class.
+ *
+ */
+class DBTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test the if the singleton function getInstance() returns MySQL and MongoDB.
+     */
+    public function testGetInstance()
+    {
+        $dbMysql = DB::getInstance('mysql');
+
+        $this->assertInstanceOf('\ActivatorAdmin\Lib\MySQL', $dbMysql);
+
+        DB::destroy();
+
+        $dbMongo = DB::getInstance('mongodb');
+
+        $this->assertInstanceOf('\ActivatorAdmin\Lib\Mongo', $dbMongo);
+    }
+}
