9b8f291c3145a5fb1a5f2e7f2285ecf01650d855
dan-lyn <daniel_lynge@hotmail.com>
2016-08-15 11:10:28 +0200

    added update function to database interface

--- lib/Database.interface.php
+++ lib/Database.interface.php
@@ -10,5 +10,6 @@ interface iDatabase
 {
     public function select($columns='*', $whereColumn=false, $whereValue=false, $orderBy=false, $limit=false);
     public function insert($data);
+    public function update($data, $whereColumn=false, $whereValue=false);
     public function delete($whereColumn, $whereValue);
 }
