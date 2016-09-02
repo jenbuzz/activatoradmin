e1cf3b999d83f5556d9168055e0437355daa909b
dan-lyn <daniel_lynge@hotmail.com>
2016-09-02 17:34:53 +0200

    added new mysql, mongo and db interface files to phpdoc script

--- docs/update_phpdoc.sh
+++ docs/update_phpdoc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-phpdoc -t phpdoc/ -d ../test/phpunit/ -f ../lib/AuthMiddleware.class.php -f ../lib/ConfigHelper.class.php -f ../lib/Item.class.php -f ../lib/Model.interface.php -f ../lib/DB.class.php -f ../lib/ModelFacade.class.php
+phpdoc -t phpdoc/ -d ../test/phpunit/ -f ../lib/AuthMiddleware.class.php -f ../lib/ConfigHelper.class.php -f ../lib/Item.class.php -f ../lib/Model.interface.php -f ../lib/Database.interface.php -f ../lib/DB.class.php -f ../lib/Mongo.class.php -f ../lib/MySQL.class.php -f ../lib/ModelFacade.class.php
