d640cc3a1f7baa44fda9c2661d51c13e43ffaf77
dan-lyn <daniel_lynge@hotmail.com>
2017-01-01 09:58:09 +0100

    setup prompting for item id to activate

--- cli/activatoradmin.js
+++ cli/activatoradmin.js
@@ -1,1 +1,9 @@
 var inquirer = require('inquirer');
+
+inquirer.prompt([{
+  type: 'input',
+  name: 'id',
+  message: 'What is the ID of the item you want to (de)activate?'
+}]).then(function (answers) {
+  console.log(answers);
+});
