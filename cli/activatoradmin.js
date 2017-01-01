eac42fa23c4c8c8fd3576a1c2974038b65f941b2
dan-lyn <daniel_lynge@hotmail.com>
2017-01-01 10:22:43 +0100

    activating items using node is now working with mysql

--- cli/activatoradmin.js
+++ cli/activatoradmin.js
@@ -1,9 +1,42 @@
 var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'activatoradmin'
+});
+connection.connect();
 
 inquirer.prompt([{
   type: 'input',
   name: 'id',
   message: 'What is the ID of the item you want to (de)activate?'
 }]).then(function (answers) {
-  console.log(answers);
+  var id = answers.id;
+
+  connection.query('SELECT * FROM items WHERE id=' + id, function (err, rows, fields) {
+    if (err) {
+      throw err;
+    }
+
+    var item = rows[0];
+
+    var isactive = item.isactive ? 0 : 1;
+
+    connection.query('UPDATE items SET isactive=' + isactive + ' WHERE id=' + id, function (err, result) {
+      if (err) {
+  		throw err;
+	  }
+
+      if (result.changedRows > 0) {
+        var msgIsActive = isactive ? 'activated' : 'deactivated';
+
+      	console.log('Item "' + item.name + '" (ID: ' + item.id + ') has been ' + msgIsActive);
+      }
+
+	  connection.end();
+    });
+  });
 });
