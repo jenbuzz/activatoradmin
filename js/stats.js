bbaeb79ca7a60b50879bc2216cd598417a5d7614
dan-lyn <daniel_lynge@hotmail.com>
2017-01-12 19:13:19 +0100

    d3 v4 requires a few modifications

--- js/stats.js
+++ js/stats.js
@@ -11,11 +11,8 @@
   var width = 500 - margin.left - margin.right;
   var height = 500 - margin.top - margin.bottom;
 
-  var x = d3.scale.ordinal().rangeRoundBands([0, width], 0.1);
-  var y = d3.scale.linear().range([height, 0]);
-
-  var xAxis = d3.svg.axis().scale(x).orient('bottom');
-  var yAxis = d3.svg.axis().scale(y).orient('left').ticks(5);
+  var x = d3.scaleBand().range([0, width]).padding(0.1);
+  var y = d3.scaleLinear().range([height, 0]);
 
   var svg = d3.select('.statsActivated')
       .append('svg')
@@ -35,11 +32,11 @@
     svg.append('g')
        .attr('class', 'x axis')
        .attr('transform', 'translate(0,' + height + ')')
-       .call(xAxis);
+       .call(d3.axisBottom(x));
 
     svg.append('g')
        .attr('class', 'y axis')
-       .call(yAxis)
+       .call(d3.axisLeft(y))
        .append('text')
        .attr('transform', 'rotate(-90)')
        .attr('y', 6)
@@ -53,7 +50,7 @@
        .append('rect')
        .attr('class', 'bar')
        .attr('x', function(d) { return x(d.name); })
-       .attr('width', x.rangeBand())
+       .attr('width', x.bandwidth())
        .attr('y', function(d) { return y(d.value); })
        .attr('height', function(d) { return height - y(d.value); })
        .append('title')
