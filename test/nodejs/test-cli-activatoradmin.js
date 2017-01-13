3a6efd1484bcadd1a65a3725efef464190fee834
dan-lyn <daniel_lynge@hotmail.com>
2017-01-13 15:14:15 +0100

    added dummy test for commandline activatoradmin

---
+++ test/nodejs/test-cli-activatoradmin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect = require('chai').expect;
+var activatoradmin = require('../../cli/activatoradmin.js');
+
+describe('activatoradmin', function() {
+  it('should ...', function() {
+    return 2;
+  });
+});
